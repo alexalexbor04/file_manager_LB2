b1a7efa7ca71551621242922d64feae3e2b06047
mishaklok <vbif.hfcjd@gmail.com>
2024-04-28 21:30:50 +0300

    Удаление файла готово. На очереди удаление папки.

--- funck misha/del_files.py
+++ funck misha/del_files.py
@@ -4,6 +4,6 @@ def del_files(files):
     for name in files:
         if os.path.isfile(name):
             os.remove(name)
-            print(f'Гатова')
+            print(f'Файл успешно удален')
         else:
-            print(f'нету')
+            print(f'Файла не существует')
