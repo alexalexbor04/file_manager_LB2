fdf85ccad4100e39f7d3b79449e07d96f0b7ace5
Sasha <alexalexbor04@gmail.com>
2024-04-28 21:51:23 +0300

    move files func finished

--- sasha_funcs/move_files_from_one_fold_to_another.py
+++ sasha_funcs/move_files_from_one_fold_to_another.py
@@ -4,14 +4,14 @@ import shutil
 def move_files_from_one_folder_to_another(first_dir, second_dir, files):
     
     if not os.path.isdir(first_dir) or not os.path.isdir(second_dir):
-        print("Такой папки нет")
+        print("Обеих папок или одной из них не существует, укажите правильные названия")
         return
 
     for file in files:
-        sourFile = os.path.join(first_dir, file)
+        moving_path = os.path.join(first_dir, file)
 
-        if os.path.isfile(sourFile):
-            shutil.move(sourFile, second_dir)
-            print(f'Расположение "{file}" изменено с "{first_dir}" на "{second_dir}"')
+        if os.path.isfile(moving_path):
+            shutil.move(moving_path, second_dir)
+            print(f'Файл {file} перемещкн из {first_dir} в {second_dir}')
         else:
-            print(f'Файла "{file}" нет в "{first_dir}"')
+            print(f'Файла {file} нет в папке {first_dir}')
